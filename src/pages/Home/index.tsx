fddc34c2b72d7b44b94a7f98d1b93410fb214bd6
harshita012345 <harshitakorat123@gmail.com>
2025-07-03 10:30:37 +0530

    style: Adjust profile image height for better responsiveness

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -31,13 +31,12 @@ const Home = () => {
                         <img
                             src="./Images/profile1.jpeg"
                             alt="Profile"
-                            className="h-screen xl:max-w-full w-full object-cover"
+                            className="h-[80vh] sm:h-screen xl:max-w-full w-full object-cover"
                         />
                     </div>
                     <div className="px-4 py-8 flex flex-col items-center md:items-start text-center md:text-left">
                         <h1 className="text-4xl md:text-5xl font-bold text-title mb-4">
-                            Welcome to
-                            <span className="text-stroke  text-transparent"> My Portfolio</span>
+                            Welcome to My Portfolio
                         </h1>
                         <p className="text-lg text-gray-700 mb-8">
                             Hi, I'm Harshita Korat, a passionate web developer.
@@ -57,14 +56,14 @@ const Home = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
                     <div className="">
                         <div>
-                            <h2 className="text-4xl md:text-5xl font-bold text-title mb-4 text-stroke  text-transparent">Introduction</h2>
+                            <h2 className="text-4xl md:text-5xl font-bold text-title mb-4">Introduction</h2>
                             <p>Hi, I'm a passionate and results-oriented MERN Stack Developer with hands-on total 4.6+ years of experience in building dynamic, full-stack web applications using MongoDB, Express.js, React.js, and Node.js.
                                 I specialize in creating scalable backend architectures, intuitive frontend interfaces, and seamless API integrations.
                                 My approach focuses on writing clean, efficient code and delivering high-performing solutions that align with both user needs and business goals.
                                 I enjoy solving complex problems and continuously enhancing my skills to stay updated with modern development practices.</p>
                         </div>
                         <div className="py-4">
-                            <h2 className="text-2xl md:text-3xl font-bold text-title mb-4 text-stroke  text-transparent">Skills</h2>
+                            <h2 className="text-2xl md:text-3xl font-bold text-title mb-4">Skills</h2>
                             <ul className="list-disc pl-5">
                                 <li>Proficient in MERN Stack (MongoDB, Express.js, React.js, Node.js)</li>
                                 <li>Experience with RESTful APIs and GraphQL</li>
@@ -112,7 +111,7 @@ const Home = () => {
                 <div id="about" className="p-5 sm:p-10 text-left">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 items-start mb-8">
                         <div className="py-4">
-                            <h2 className="text-4xl md:text-5xl font-bold text-title mb-4 text-stroke  text-transparent">About Me</h2>
+                            <h2 className="text-4xl md:text-5xl font-bold text-title mb-4">About Me</h2>
                         </div>
                         <div>
                             <h4 className="text-xl md:text-2xl font-bold text-title mb-4">My Vision</h4>
@@ -132,7 +131,7 @@ const Home = () => {
             <section id="career" className="section-animate">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
                     <div className="p-5 sm:p-10 text-left">
-                        <h3 className="text-4xl md:text-5xl font-bold text-title mb-4 text-stroke  text-transparent">Career</h3>
+                        <h3 className="text-4xl md:text-5xl font-bold text-title mb-4">Career</h3>
                         <p>Throughout my career as a MERN Stack Developer, I have contributed to the development of
                             multiple real-world applications across domains such as e-commerce, HRMS, and finance. I’ve built full-stack solutions from the ground up using technologies like React.js, Next.js, Node.js, NestJS, MongoDB, and PostgreSQL.
                             My work includes building dynamic e-commerce platforms with integrated payment gateways,
@@ -141,7 +140,7 @@ const Home = () => {
                     </div>
                     <div>
                         <img
-                            src="./Images/career.jpg"
+                            src="./Images/career1.jpg"
                             alt="About Me"
                             className="w-full object-cover"
                         />
@@ -152,7 +151,7 @@ const Home = () => {
             {/* projects */}
             <section id="projects" className="bg-gray-100 section-animate">
                 <div className="p-5 sm:p-10 text-left">
-                    <h4 className="text-4xl md:text-5xl font-bold text-title mb-4 text-stroke  text-transparent">Projects</h4>
+                    <h4 className="text-4xl md:text-5xl font-bold text-title mb-4">Projects</h4>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                         <div>
                             <h1><strong>🧑‍💼 Crest-Connect & LMS (HRMS + Learning Management System)</strong></h1>
@@ -217,7 +216,7 @@ const Home = () => {
                             />
                         </div>
                         <div>
-                            <h2 className="text-4xl md:text-5xl font-bold text-title mb-4 text-stroke  text-transparent">Contact Me</h2>
+                            <h2 className="text-4xl md:text-5xl font-bold text-title mb-4">Contact Me</h2>
                             <p>I'm always open to discussing new projects, collaboration opportunities, or freelance work. Whether you have an idea you'd like to bring to life or just want to connect, feel free to reach out! You can contact me via email or through the form below — I'll get back to you as soon as possible.</p>
                             <ul className="space-y-4 text-lg py-3">
                                 <li className="flex items-center gap-3">
@@ -226,7 +225,7 @@ const Home = () => {
                                 </li>
                                 <li className="flex items-center gap-3">
                                     <span className="text-xl">🌐</span>
-                                    <span>www.reallygreatsite.com</span>
+                                    <span>https://portfolio-harshita-five.vercel.app/</span>
                                 </li>
                                 <li className="flex items-center gap-3">
                                     <span className="text-xl">📧</span>
@@ -249,7 +248,7 @@ const Home = () => {
             {/* Thank you */}
             <section className="bg-gray-100 section-animate" id="thankyou">
                 <div className="p-5 sm:p-10 text-center">
-                    <h6 className="text-4xl md:text-5xl font-bold text-title mb-4 text-stroke  text-transparent">Thank You!</h6>
+                    <h6 className="text-4xl md:text-5xl font-bold text-title mb-4">Thank You!</h6>
                     <p>Thank you for taking the time to explore my portfolio. I truly appreciate your interest in my work and journey as a MERN Stack Developer. I’m passionate about building meaningful, user-focused applications and always eager to take on new challenges. If my experience aligns with your goals, I’d love to connect and collaborate.</p>
                 </div>
             </section>
